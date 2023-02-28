200a402188af92906e4279c0d66ea259dd38cd0c
rumia0601 <rumia0601@gmail.com>
2023-02-28 23:41:40 +0900

    Log System Added

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,6 +4,7 @@ from ..models import Question
 from django.db.models import Q, Count
 
 def index(request):
+    3/0
     page = request.GET.get("page", "1")
     kw = request.GET.get("kw", "")
     so = request.GET.get("so", "recent")
